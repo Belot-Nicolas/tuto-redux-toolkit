6b05c4caa6906f86ce5341e6a455eb1a15a3a980
belot nicolas <nicolas-belot_student2021@wilder.school>
2022-06-27 18:41:13 +0200

    tuto redux/toolkit

---
+++ src/redux.js
@@ -0,0 +1,51 @@
+import {
+  configureStore,
+  createSlice,
+} from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState: [
+    { id: 1, text: "Faire les courses", done: false },
+    { id: 2, text: "Ménage !", done: true },
+  ],
+  reducers: {
+    addTask: (state, action) => {
+      // {type: "todo/addTask" (<= a appelé pour utilisé l'action) , PAYLOAD: "aller faire les courses"}
+      const newTask = {
+        id: Date.now(),
+        done: false,
+        text: action.payload,
+      };
+      state.push(newTask);
+    },
+    toggleTask: (state, action) => {
+      // {type: "todo/toggleTask", payload: 20 (c'est l'id) }
+      const task = state.find((tache) => tache.id === action.payload);
+      task.done = !task.done;
+    },
+    deleteTask: (state, action) => {
+      // {type: "tod/deleteTask", payload: 20 (c'est l'id) }
+      state = state.filter((t) => t.id !== action.payload);
+      return state
+    },
+  },
+});
+
+export const { addTask, deleteTask, toggleTask } = todoSlice.actions; // c'est + ou - la const du dessous
+
+// action creator
+// export const createToggle = (id) => {
+//     return {
+//       type: "todo/toggleTask",
+//       payload: id,
+//     };
+//   };
+
+export const store = configureStore({
+  reducer: {
+    todo: todoSlice.reducer,
+  },
+});
+
+
